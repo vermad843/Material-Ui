964a734503486b14173b20ddaafccbbb8293f879
vermad843 <vishnuverma7524@gmail.com>
2021-03-08 03:39:04 +0530

    checkbox

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true)
+  return (
+    <FormControlLabel
+       control = {
+         <Checkbox
+           checked = {checked}
+           onChange = {(e) => setChecked(e.target.checked)}
+           inputProps = {{
+             'aria-label' : 'secondary checkbox'
+           }}
+         />}
+         label = "Testing Checkbox"
+    />
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ButtonGroup>
+        <CheckboxExample/>
+        <ButtonGroup variant = "contained" color="primary">
            <Button
                startIcon = {<SaveIcon/>}
-              //  endIcon = {<SaveIcon/>}
-               size = "large"
-               variant="contained" 
-               color = "primary">
+              >
               <h2>Save</h2>
            </Button>
            <Button
                startIcon = {<DeleteIcon/>}
-              //  endIcon = {<SaveIcon/>}
-               size = "large"
-               variant="contained" 
-               color = "secondary">
+              >
               <h2>Discard</h2>
            </Button>
         </ButtonGroup>
